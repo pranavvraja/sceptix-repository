c641637409c8e8c0750590a7b1e4680d2f684fc2
pranavvraja <pranavvraja@gmail.com>
2023-02-16 21:57:28 +0530

    Completed typing animation

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 var myVar;
-
 function myFunction() {
-  myVar = setTimeout(showPage, 3000);
+  myVar = setTimeout(showPage, 3050);
 }
-
 function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("header").style.display = "block";
+}
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("actice-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("actice-tab");
 }
