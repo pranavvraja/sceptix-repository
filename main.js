3f3f0b839b641da2d166e3c51510258b77575dbb
pranavvraja <pranavvraja@gmail.com>
2023-02-17 19:30:15 +0530

    removed extra lines

--- main.js
+++ main.js
@@ -20,3 +20,23 @@ function opentab(tabname) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("actice-tab");
 }
+
+
+
+
+window.onscroll = function() {myFunction1()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction1() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+} 
