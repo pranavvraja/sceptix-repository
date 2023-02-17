15ffb8d32dd0081b07e868c5a7903c6c86beaf77
pranaam <pranaam541@gmail.com>
2023-02-17 20:16:59 +0530

    Revert "Merge branch 'main' of https://github.com/pranavvraja/sceptix-repository"
    This reverts commit 13c2639bf8c83b5b70e96cb24923224a33caf1cb, reversing
    changes made to f7ee9250d621aaefdd4da583486b79ea89d98435.

--- main.js
+++ main.js
@@ -26,9 +26,17 @@ function opentab(tabname) {
 
 window.onscroll = function() {myFunction1()};
 
-function myFunction1(){
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction1() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+} 
