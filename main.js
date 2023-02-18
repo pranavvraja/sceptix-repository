13d1cfe5e2795949e47a7ec23284c1397d5e7306
pranavvraja <pranavvraja@gmail.com>
2023-02-18 11:42:21 +0530

    formatted the files

--- main.js
+++ main.js
@@ -11,22 +11,22 @@ function showPage() {
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 function opentab(tabname) {
-    for (tablink of tablinks) {
-        tablink.classList.remove("active-link");
-    }
-    for (tabcontent of tabcontents) {
-        tabcontent.classList.remove("actice-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("actice-tab");
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("actice-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("actice-tab");
 }
 
 
 
 
-window.onscroll = function() {myFunction1()};
+window.onscroll = function () { myFunction1() };
 
-function myFunction1(){
+function myFunction1() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
